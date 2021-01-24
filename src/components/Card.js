9694eb970789e7d3115966c506e308ba86e1179c
Prashant Lonkar <prashant.lonkar@outlook.com>
2021-01-24 14:27:43 +0530

    make cards render properly on all screens

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,11 @@
 import React from "react";
+import "../App.css";
 import "hover.css/css/hover.css";
 
 const Card = ({ name, info }) => {
   return (
-    <div className="card hvr-hang hvr-shadow">
-      <img
-        src={`https://robohash.org/${name}`}
-        alt="robot-faces"
-        width="150"
-        height="100"
-      />
+    <div className="card hvr-hang">
+      <img src={`https://robohash.org/${name}`} alt="robot-faces" />
       <div>
         <h2 className="title">{name}</h2>
         <p className="info">{info}</p>
